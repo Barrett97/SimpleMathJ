bc436257c1f320fa57333ef25c8dab5dcf01ba8f
Barret <tranbarrett@gmail.com>
2020-10-05 16:40:56 -0400

    Fragment updates

--- app/src/main/java/com/example/simplemathj/MainActivity.java
+++ app/src/main/java/com/example/simplemathj/MainActivity.java
@@ -22,6 +22,18 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
