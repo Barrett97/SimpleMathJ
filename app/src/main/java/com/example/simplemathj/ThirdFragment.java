22b4ac3d22cf9b2a515b5b45490a5b2f2f72db3c
Barret <tranbarrett@gmail.com>
2020-10-06 18:34:27 -0400

    Update single digit mult fragment

--- app/src/main/java/com/example/simplemathj/ThirdFragment.java
+++ app/src/main/java/com/example/simplemathj/ThirdFragment.java
@@ -4,10 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.StringRes;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+
+import java.lang.reflect.Array;
+import java.util.List;
+import java.util.Random;
 
 public class ThirdFragment extends Fragment {
 
@@ -23,6 +30,14 @@ public class ThirdFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+
+
+//        RelativeLayout layout = view.findViewById(R.id.thirdFragLayout);
+//        int equationPosition = getResources().getDisplayMetrics().heightPixels/3;
+        EditText eq = view.findViewById(R.id.editTextNumber);
+        eq.setText(generateNumbers() + " X " + generateNumbers());
+
+
 //        view.findViewById(R.id.axb).setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View view) {
@@ -31,4 +46,10 @@ public class ThirdFragment extends Fragment {
 //            }
 //        });
     }
+
+    private String generateNumbers() {
+        Random random = new Random();
+        return Integer.toString(random.nextInt(9));
+    }
+
 }
