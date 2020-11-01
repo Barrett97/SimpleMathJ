bc03e8d2331860a5350f8701973c07bb3379715f
Barret <tranbarrett@gmail.com>
2020-10-31 20:03:10 -0400

    Test class

---
+++ app/src/test/java/com/example/simplemathj/MyActivityTest.kt
@@ -0,0 +1,11 @@
+package com.example.simplemathj
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+class MyActivityTest {
+    @Test
+    fun testEvent() {
+        val scenario = launchActivity<MainActivity>;
+    }
+}
